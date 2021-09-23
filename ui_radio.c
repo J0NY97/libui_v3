070b4513fa0cfcbb6d16dafcb56df4d0df9b5417
J0NY97 <jony.salmi97@gmail.com>
2021-09-23 18:23:46 +0300

    maybe we should add the new event handler to the radio, which is basically the same

--- ui_radio.c
+++ ui_radio.c
@@ -14,31 +14,13 @@ void	ui_radio_new(t_ui_window *win, t_ui_element *elem)
 
 void	ui_radio_event(t_ui_element *elem, SDL_Event e)
 {
-	t_ui_element	*child;
 	t_ui_radio		*radio;
 	t_list			*curr;
 
 	if (!elem->show || !*elem->parent_show)
 		return ;
 	radio = elem->element;
-	curr = elem->children;
-	while (curr)
-	{
-		child = curr->content;
-		child->is_toggle = 0;
-		if (child->was_click)
-			radio->active = child;
-		else if (child->is_hover)
-			child->state = UI_STATE_HOVER;
-		else
-			child->state = UI_STATE_DEFAULT;
-		curr = curr->next;
-	}
-	if (radio->active)
-	{
-		radio->active->state = UI_STATE_CLICK;
-		radio->active->is_toggle = 1;
-	}
+	ui_list_radio_event(elem->children, &radio->active, e);
 }
 
 /*
