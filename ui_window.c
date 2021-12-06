58657aa1dc87c80b6673aac8b2cabb8f178f7bec
J0NY97 <jony.salmi97@gmail.com>
2021-12-06 20:04:40 +0200

    ui_window normed

--- ui_window.c
+++ ui_window.c
@@ -8,16 +8,14 @@ void	ui_window_new(t_ui_window *win, char *title, t_vec4 pos)
 	else
 		win->title = ft_strdup(title);
 	win->pos = pos;
-	win->win = SDL_CreateWindow(win->title, win->pos.x, win->pos.y, win->pos.w, win->pos.h, 0);
+	win->win = SDL_CreateWindow(win->title,
+			win->pos.x, win->pos.y, win->pos.w, win->pos.h, 0);
 	win->renderer = SDL_CreateRenderer(win->win, -1, SDL_RENDERER_ACCELERATED);
-	// NOTE : Default create same sized texture as window;
 	win->texture = SDL_CreateTexture(win->renderer, SDL_PIXELFORMAT_RGBA8888,
-		SDL_TEXTUREACCESS_TARGET, win->pos.w, win->pos.h);
+			SDL_TEXTUREACCESS_TARGET, win->pos.w, win->pos.h);
 	win->show = 1;
 	win->children = NULL;
-
 	win->window_id = SDL_GetWindowID(win->win);
-
 	win->screen_pos.w = pos.w;
 	win->screen_pos.h = pos.h;
 	win->texture_scale = vec2(win->screen_pos.w / win->pos.w,
@@ -29,63 +27,82 @@ void	ui_window_new(t_ui_window *win, char *title, t_vec4 pos)
 	win->free_me = 1;
 }
 
-void	ui_window_event(t_ui_window *win, SDL_Event e)
+void	ui_window_resize_event(t_ui_window *win, SDL_Event e)
 {
-	if (win->window_id == e.window.windowID)
+	t_vec2i	new_pos;
+
+	SDL_GetWindowSize(win->win, &new_pos.x, &new_pos.y);
+	win->pos.w = new_pos.x;
+	win->pos.h = new_pos.y;
+	win->texture_scale = vec2(win->screen_pos.w / win->pos.w,
+			win->screen_pos.h / win->pos.h);
+	win->textures_recreate = 1;
+	ft_printf("[%s] Window resized.\n", __FUNCTION__);
+}
+
+void	ui_window_window_events(t_ui_window *win, SDL_Event e)
+{
+	if (e.window.event == SDL_WINDOWEVENT_RESIZED)
+		ui_window_resize_event(win, e);
+	else if (e.window.event == SDL_WINDOWEVENT_CLOSE)
 	{
-		if (e.type == SDL_WINDOWEVENT)
+		if (win->hide_on_x)
+			ui_window_flag_set(win, UI_WINDOW_HIDE);
+		else
+			win->wants_to_close = 1;
+	}
+	else if (e.window.event == SDL_WINDOWEVENT_SHOWN)
+		win->show = 1;
+	else if (e.window.event == SDL_WINDOWEVENT_HIDDEN)
+		win->show = 0;
+}
+
+void	ui_window_mouse_events(t_ui_window *win, SDL_Event e)
+{
+	SDL_GetMouseState(&win->window_mouse_pos.x, &win->window_mouse_pos.y);
+	win->mouse_pos.x = win->window_mouse_pos.x * win->texture_scale.x;
+	win->mouse_pos.y = win->window_mouse_pos.y * win->texture_scale.y;
+}
+
+void	ui_window_keydown_events(t_ui_window *win, SDL_Event e)
+{
+	int	modkey;
+
+	modkey = SDL_GetModState();
+	if (modkey & KMOD_LALT)
+	{
+		if (e.key.keysym.scancode == SDL_SCANCODE_RETURN)
 		{
-			if (e.window.event == SDL_WINDOWEVENT_RESIZED)
-			{
-				t_vec2i	new_pos;
-				SDL_GetWindowSize(win->win, &new_pos.x, &new_pos.y);
-				win->pos.w = new_pos.x;
-				win->pos.h = new_pos.y;
-				win->texture_scale = vec2(win->screen_pos.w / win->pos.w,
-						win->screen_pos.h / win->pos.h);
-				win->textures_recreate = 1;
-			}
-			else if (e.window.event == SDL_WINDOWEVENT_CLOSE)
-			{
-				if (win->hide_on_x)
-					ui_window_flag_set(win, UI_WINDOW_HIDE);
-				else
-					win->wants_to_close = 1;
-			}
-		}
-		else if (e.type == SDL_MOUSEMOTION)
-		{
-			SDL_GetMouseState(&win->window_mouse_pos.x, &win->window_mouse_pos.y);
-			win->mouse_pos.x = win->window_mouse_pos.x * win->texture_scale.x;
-			win->mouse_pos.y = win->window_mouse_pos.y * win->texture_scale.y;
-		}
-		else if (e.type == SDL_MOUSEWHEEL)
-			win->scroll = e.wheel.y;
-		else if (e.type == SDL_KEYDOWN)
-		{
-			int modkey = SDL_GetModState();
-			if (modkey & KMOD_LALT) // LALT mods
-			{
-				if (e.key.keysym.scancode == SDL_SCANCODE_RETURN) // make fullscreen
-				{
-					ui_window_flag_set(win, UI_WINDOW_FULLSCREEN);
-					win->textures_recreate = 1;
-				}
-			}
-			else if (modkey & KMOD_LCTRL) // LCTRL mods
-			{
-				if (e.key.keysym.scancode == SDL_SCANCODE_R) // force texture recreate
-					win->textures_recreate = 1;
-			}
-		}
-		if (e.button.type == SDL_MOUSEBUTTONDOWN)
-		{
-			win->mouse_down = e.button.button;
-			win->mouse_down_last_frame = win->mouse_down;
+			ui_window_flag_set(win, UI_WINDOW_FULLSCREEN);
+			ui_window_resize_event(win, e);
 		}
 	}
+	else if (modkey & KMOD_LCTRL)
+	{
+		if (e.key.keysym.scancode == SDL_SCANCODE_R)
+			win->textures_recreate = 1;
+	}
+}
+
+void	ui_window_event(t_ui_window *win, SDL_Event e)
+{
 	if (e.button.type == SDL_MOUSEBUTTONUP)
 		win->mouse_down = 0;
+	if (win->window_id != e.window.windowID)
+		return ;
+	if (e.type == SDL_WINDOWEVENT)
+		ui_window_window_events(win, e);
+	else if (e.type == SDL_MOUSEMOTION)
+		ui_window_mouse_events(win, e);
+	else if (e.type == SDL_MOUSEWHEEL)
+		win->scroll = e.wheel.y;
+	else if (e.type == SDL_KEYDOWN)
+		ui_window_keydown_events(win, e);
+	if (e.button.type == SDL_MOUSEBUTTONDOWN)
+	{
+		win->mouse_down = e.button.button;
+		win->mouse_down_last_frame = win->mouse_down;
+	}
 }
 
 int	ui_window_render(t_ui_window *win)
@@ -94,21 +111,16 @@ int	ui_window_render(t_ui_window *win)
 
 	SDL_SetRenderTarget(win->renderer, NULL);
 	SDL_RenderCopy(win->renderer, win->texture, NULL, NULL);
-
 	SDL_SetRenderTarget(win->renderer, win->texture);
 	rgba = hex_to_rgba(win->bg_color);
 	SDL_SetRenderDrawColor(win->renderer, rgba.r, rgba.g, rgba.b, rgba.a);
 	SDL_RenderClear(win->renderer);
-
 	SDL_SetRenderTarget(win->renderer, NULL);
 	win->textures_recreate = 0;
-
-	// Resetting events, becuase sdl wont go inside the event function,if NO events have been done.
 	win->mouse_pos_prev = win->mouse_pos;
 	win->scroll = 0;
 	win->mouse_down_last_frame = 0;
-
-	SDL_RenderPresent(win->renderer); // only do this if not handled
+	SDL_RenderPresent(win->renderer);
 	return (1);
 }
 
@@ -136,10 +148,9 @@ void	ui_window_free(void *window, size_t size)
 	ft_printf("me\n");
 }
 
-/*
+/*********************************************
  * Window edititing functions down from here.
-*/
-
+**********************************************/
 void	ui_window_texture_redo(t_ui_window *win)
 {
 	if (!win)
@@ -181,26 +192,28 @@ void	ui_window_texture_pos_set(t_ui_window *win, t_vec2i pos)
 {
 	if (pos.x != win->screen_pos.w || pos.y != win->screen_pos.h)
 	{
-		win->screen_pos = vec4i(win->screen_pos.x, win->screen_pos.y, pos.x, pos.y);
+		win->screen_pos
+			= vec4i(win->screen_pos.x, win->screen_pos.y, pos.x, pos.y);
 		ui_window_texture_redo(win);
 	}
 }
 
-void	ui_window_flag_set(t_ui_window *win, int flags)
+int	choose_fullscreen_mode(t_ui_window *win)
 {
 	int	curr_flags;
 
 	curr_flags = SDL_GetWindowFlags(win->win);
+	if (curr_flags & SDL_WINDOW_FULLSCREEN)
+		return (0);
+	return (SDL_WINDOW_FULLSCREEN);
+}
+
+void	ui_window_flag_set(t_ui_window *win, int flags)
+{
 	if (flags & UI_WINDOW_HIDE)
-	{
 		SDL_HideWindow(win->win);
-		win->show = 0;
-	}
 	if (flags & UI_WINDOW_SHOW)
-	{
 		SDL_ShowWindow(win->win);
-		win->show = 1;
-	}
 	if (flags & UI_WINDOW_RAISE)
 		SDL_RaiseWindow(win->win);
 	if (flags & UI_WINDOW_MAXIMIZE)
@@ -208,12 +221,7 @@ void	ui_window_flag_set(t_ui_window *win, int flags)
 	if (flags & UI_WINDOW_MINIMIZE)
 		SDL_MinimizeWindow(win->win);
 	if (flags & UI_WINDOW_FULLSCREEN)
-	{
-		if (curr_flags & SDL_WINDOW_FULLSCREEN)
-			SDL_SetWindowFullscreen(win->win, 0);
-		else
-			SDL_SetWindowFullscreen(win->win, SDL_WINDOW_FULLSCREEN);
-	}
+		SDL_SetWindowFullscreen(win->win, choose_fullscreen_mode(win));
 	if (flags & UI_WINDOW_FULLSCREEN_WINDOWED)
 		SDL_SetWindowFullscreen(win->win, SDL_WINDOW_FULLSCREEN_DESKTOP);
 	if (flags & UI_WINDOW_GRAB)
@@ -241,8 +249,11 @@ void	ui_window_id_set(t_ui_window *win, const char *id)
 
 /*
  * This function should probably be called right after loading the ui,
- * otherwise you might save the ui_window to a variable that is used somewhere else,
- * or renderer...
+ * otherwise you might save the ui_window to a variable that is used
+ * somewhere else, or renderer...
+ *
+ * NOTE:
+ * 	1. we will sdl_win associated renderer if it has one, otherwise make one;
 */
 void	ui_window_replace_win(t_ui_window *ui_win, SDL_Window *sdl_win)
 {
@@ -259,16 +270,15 @@ void	ui_window_replace_win(t_ui_window *ui_win, SDL_Window *sdl_win)
 		SDL_DestroyRenderer(ui_win->renderer);
 	ui_win->renderer = SDL_GetRenderer(sdl_win);
 	if (!ui_win->renderer)
-		ui_win->renderer = SDL_CreateRenderer(sdl_win, -1, SDL_RENDERER_ACCELERATED);
+		ui_win->renderer
+			= SDL_CreateRenderer(sdl_win, -1, SDL_RENDERER_ACCELERATED);
 	else
-	{
-		ft_printf("[%s] The window already had a renderer.\n", __FUNCTION__);
 		ui_win->renderer_replaced = 1;
-	}
 	if (ui_win->texture)
 		SDL_DestroyTexture(ui_win->texture);
-	ui_win->texture = SDL_CreateTexture(ui_win->renderer, SDL_PIXELFORMAT_RGBA8888,
-		SDL_TEXTUREACCESS_TARGET, ui_win->pos.w, ui_win->pos.h);
+	ui_win->texture
+		= SDL_CreateTexture(ui_win->renderer, SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_TARGET, ui_win->pos.w, ui_win->pos.h);
 	ui_win->window_id = SDL_GetWindowID(sdl_win);
 }
 
