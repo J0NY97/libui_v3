bd67ad4de1544e9f346082ad763e4e87697a1d2a
J0NY97 <jony.salmi97@gmail.com>
2021-12-10 17:52:51 +0200

    ui_element normed

---
+++ ui_element6.c
@@ -0,0 +1,38 @@
+#include "libui.h"
+
+/*
+ * We have this only so that we wouldnt have errors spamming the terminal.
+*/
+void	ui_element_event(t_ui_element *elem, SDL_Event e)
+{
+	(void)elem;
+	(void)e;
+}
+
+int	ui_element_is_hover(t_ui_element *elem)
+{
+	if (elem->show
+		&& elem->rendered_last_frame
+		&& *elem->parent_show // Why was this commented out?;
+		&& point_in_rect(elem->win->mouse_pos, elem->to_pos))
+		return (1);
+	return (0);
+}
+
+/*
+ * Was the element hovered last frame.
+*/
+int	ui_element_was_hover(t_ui_element *elem)
+{
+	if (elem->rendered_last_frame
+		&& point_in_rect(elem->win->mouse_pos_prev, elem->to_pos))
+		return (1);
+	return (0);
+}
+
+int	ui_element_is_click(t_ui_element *elem)
+{
+	if (ui_element_is_hover(elem) && elem->win->mouse_down)
+		return (1);
+	return (0);
+}
