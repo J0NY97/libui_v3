88107bf90954ce98df9774b77932e29ff1d41544
J0NY97 <jony.salmi97@gmail.com>
2021-12-10 17:40:43 +0200

    ui_button normed

--- ui_button.c
+++ ui_button.c
@@ -46,27 +46,6 @@ void	ui_button_event(t_ui_element *elem, SDL_Event e)
 		elem->state = UI_STATE_CLICK;
 }
 
-void	ui_button_toggle_on(t_ui_element *elem)
-{
-	if (elem)
-	{
-		elem->is_toggle = 1;
-		elem->state = UI_STATE_CLICK;
-	}
-}
-
-bool	ui_button(t_ui_element *elem)
-{
-	if (elem->is_click)
-	{
-		elem->is_click = 0;
-		elem->is_hover = 0;
-		elem->state = UI_STATE_CLICK;
-		return (1);
-	}
-	return (0);
-}
-
 /*
  * NOTE: !!!!! Dont do anything that needs the element type to be UI_TYPE_BUTTOn
  * since our dropdown is using this button_redner for reason.!!!!!!!!!!!!!!!!!!!
@@ -85,31 +64,6 @@ int	ui_button_render(t_ui_element *elem)
 	return (1);
 }
 
-/*
- * Returns from button element its child elements
- * 	(that are specific to this type of element only);
-*/
-t_ui_element	*ui_button_get(t_ui_element *elem, int ui_type)
-{
-	if (elem->element_type != UI_TYPE_BUTTON)
-		return (NULL);
-	if (ui_type == UI_TYPE_LABEL)
-		return (ui_button_get_label_element(elem));
-	return (NULL);
-}
-
-t_ui_button	*ui_button_get_button(t_ui_element *elem)
-{
-	if (elem->element_type != UI_TYPE_BUTTON)
-	{
-		ft_printf("[%s] Elem is not of type button. <%d : %s>\n",
-			__FUNCTION__, elem->element_type,
-			ui_element_type_to_string(elem->element_type));
-		return (NULL);
-	}
-	return (elem->element);
-}
-
 void	ui_button_free(void *elem, size_t size)
 {
 	t_ui_element	*element;
@@ -123,87 +77,4 @@ void	ui_button_free(void *elem, size_t size)
 		return ;
 	free(button);
 	(void)size;
-}
-
-void	ui_button_print(t_ui_element *elem)
-{
-	t_ui_button	*button;
-
-	if (elem->element_type != UI_TYPE_BUTTON)
-	{
-		ft_printf("[%s] Not elem of type button. <%d : %s>",
-			__FUNCTION__, elem->element_type,
-			ui_element_type_to_string(elem->element_type));
-		return ;
-	}
-	button = ui_button_get_button(elem);
-	if (!button)
-		return ;
-	ui_label_print(&button->label);
-}
-
-/*
- * Getters
-*/
-t_ui_element	*ui_button_get_label_element(t_ui_element *elem)
-{
-	t_ui_button	*button;
-
-	if (!elem)
-	{
-		ft_printf("[%s] No element.\n", __FUNCTION__);
-		return (NULL);
-	}
-	if (elem->element_type != UI_TYPE_BUTTON)
-	{
-		ft_printf("[%s] Element not of type UI_TYPE_BUTTON. It\'s %d %s.",
-			__FUNCTION__, elem->element_type,
-			ui_element_type_to_string(elem->element_type));
-		return (NULL);
-	}
-	button = elem->element;
-	if (!button)
-		return (NULL);
-	return (&button->label);
-}
-
-t_ui_label	*ui_button_get_label(t_ui_element *elem)
-{
-	t_ui_element	*label;
-
-	label = ui_button_get_label_element(elem);
-	if (!label)
-		return (NULL);
-	return (label->element);
-}
-
-void	ui_button_set_text(t_ui_element *elem, char *str)
-{
-	t_ui_element		*label;
-
-	label = ui_button_get_label_element(elem);
-	ui_label_set_text(label, str);
-}
-
-char	*ui_button_get_text(t_ui_element *elem)
-{
-	t_ui_element		*label;
-
-	label = ui_button_get_label_element(elem);
-	return (ui_label_get_text(label));
-}
-
-/*
- * From list get the button with str as text;
-*/
-t_ui_element	*ui_list_get_button_with_text(t_list *list, const char *str)
-{
-	while (list)
-	{
-		if (ft_strequ(ui_button_get_text(list->content), str))
-			return (list->content);
-		list = list->next;
-	}
-	ft_printf("[%s] No button with text %s in list.\n", __FUNCTION__, str);
-	return (NULL);
 }
