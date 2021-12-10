f997bce24dae0368b66aaa5fa92858378d1a60d2
J0NY97 <jony.salmi97@gmail.com>
2021-12-10 18:13:15 +0200

    ui_radio normed

--- ui_radio.c
+++ ui_radio.c
@@ -84,32 +84,3 @@ void	ui_radio_free(void *elem, size_t size)
 	free(radio);
 	(void)size;
 }
-
-/*
- * Other functions
-*/
-
-/*
- * if 'toggle_this' is in radio button list it will toggle it on,
- * and untoggle all the other buttons;
-*/
-void	ui_radio_button_toggle_on(t_ui_element *elem, t_ui_element *toggle_this)
-{
-	t_ui_element	*child;
-	t_ui_radio		*radio;
-	t_list			*curr;
-
-	radio = elem->element;
-	curr = elem->children;
-	while (curr)
-	{
-		child = curr->content;
-		if (toggle_this == child)
-			radio->active = toggle_this;
-		else
-			child->state = UI_STATE_DEFAULT;
-		curr = curr->next;
-	}
-	if (radio->active)
-		radio->active->state = UI_STATE_CLICK;
-}
